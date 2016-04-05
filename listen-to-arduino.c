98cc6f1ace7323c9f52cfe1432de3fc120902148
Ethan <ethanabrooks@gmail.com>
2016-04-05 16:47:33 -0400

    fixed get_extrema bug

--- listen-to-arduino.c
+++ listen-to-arduino.c
@@ -1,4 +1,3 @@
-#include "read-temperature.h"
 #include "listen-to-arduino.h"
 #include "queue.h"
 #include <fcntl.h>
@@ -16,7 +15,24 @@
 extern float average, min, max;
 extern pthread_mutex_t lock;
 
-void read_temperature(char *temp_buff) {
+void read_temperature(int fd, char *temp_buff) {
+
+  // int newline_count = 0;
+  int total_bytes = 0;
+
+  // clear local_temp buff
+  bzero(temp_buff, TEMP_MSG_LENGTH);
+
+  //Do not exceed reading longer than the length of the msg
+  while(temp_buff[total_bytes - 1] != '\n') {
+    //Read only one byte at a time and only execute block if a byte is received
+    total_bytes += read(fd, &temp_buff[total_bytes], 1);
+  }
+}
+
+void* listen_to_arduino(void* argv) {
+
+  /* configure connection to Arduino */
 
   //Specific to macs and our team's arduino device
   int fd = open("/dev/cu.usbmodem1411", O_RDWR);
@@ -33,39 +49,23 @@ void read_temperature(char *temp_buff) {
   cfsetospeed(&options, 9600); // set output baud rate
   tcsetattr(fd, TCSANOW, &options); // set options
 
-  // int newline_count = 0;
-  int total_bytes = 0;
-    
-    //Reset counting variables
-    total_bytes = 0;
 
-    // clear local_temp buff
-    bzero(temp_buff, TEMP_MSG_LENGTH);
-
-    //Do not exceed reading longer than the length of the msg
-    while(temp_buff[total_bytes - 1] != '\n') {
-      //Read only one byte at a time and only execute block if a byte is received
-      total_bytes += read(fd, &temp_buff[total_bytes], 1);
-    }
-    close(fd);
-}
-
-void* listen_to_arduino(void* argv) {
   queue_t* q = new_queue(SECS_PER_HOUR);
   int num = 0; // for computing average
   char temp_buff [BUFF_SIZE]; // holds strings read from Arduino
   while(1) {
-    read_temperature(temp_buff); // save string from Ardunito to temp_buff
-    float temp = atoi(temp_buff);
+    read_temperature(fd, temp_buff); // save string from Ardunito to temp_buff
+    float temp = atof(temp_buff);
     enqueue(q, temp);
 
     num++;
     pthread_mutex_lock(&lock);
     get_extrema(q, &min, &max); // get min and max values in queue
     average = (average * (num - 1) + temp) / num; // update average
-    printf("temp: %f\nmin: %f\nmax: %f\naverage: %f",
+    printf("temp: %f\nmin: %f\nmax: %f\naverage: %f\n",
         temp, min, max, average);
     pthread_mutex_unlock(&lock);
   }
+  close(fd);
   delete_queue(q);
 }
