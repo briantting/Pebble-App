b46c81c50893b41ffe387f4d8870bb320f2eb3f1
Ethan <ethanabrooks@gmail.com>
2016-04-04 23:18:35 -0400

    added listen-to-arduino

--- main.c
+++ main.c
@@ -9,19 +9,19 @@ http://www.binarii.com/files/papers/c_sockets.txt
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include "read_temperature.h"
+#include "read-temperature.h"
 #include "listen-to-pebble.h"
 #include "listen-to-arduino.h"
 #include "server.h"
-#define BUFF_SIZE 10000
 
-char temp_buff [BUFF_SIZE] = "test";
-char* temperature = temp_buff;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 int error() { 
   return -1;
 }
+
+float average = 0;
+float min, max;
 
 int main(int argc, char *argv[])
 {
@@ -39,7 +39,6 @@ int main(int argc, char *argv[])
 
   puts("Enter 'q' at any time to quit.");
   pthread_t t1, t2;
-  /*start_server(port_number, argv[2]);*/
   if ( pthread_create( &t1, NULL, listen_to_arduino, argv) != 0  ) {
     return error(); 
   }
