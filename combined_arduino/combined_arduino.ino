b89b202fb4a234519db6e90bb28ecd0531095ea1
Brian Ting <brian.t.ting@gmail.com>
2016-04-21 16:08:09 -0400

    toggle

--- combined_arduino/combined_arduino.ino
+++ combined_arduino/combined_arduino.ino
@@ -54,6 +54,8 @@ void loop() {
   bool setArm = false;
   unsigned long setArmTime;
   unsigned long setDisarmTime;
+
+  bool standby = false;
   
   /* Configure 7-Segment to 12mA segment output current, Dynamic mode, 
      and Digits 1, 2, 3 AND 4 are NOT blanked */
@@ -99,6 +101,8 @@ void loop() {
     } else if (IncomingByte == 'd') {
       alarm = 'd';
       Serial.print("a: d\n");
+    } else if (IncomingByte == 't') {
+      standby = !standby;
     }
 
     // starts set arm countdown
@@ -135,7 +139,7 @@ void loop() {
     SerialMonitorPrint (Temperature_H, Decimal, IsPositive);
     
     /* Display temperature on the 7-Segment */
-    if (!setArm && alarm != 't') {
+    if (!setArm && alarm != 't' && !standby) {
       Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, IsCelsius);
     }
     
