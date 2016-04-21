ca1c8e1feb7a7dd4b29dbda086c332942ca4f5f8
Brian Ting <brian.t.ting@gmail.com>
2016-04-21 16:24:12 -0400

    blank display

--- combined_arduino/combined_arduino.ino
+++ combined_arduino/combined_arduino.ino
@@ -1,4 +1,4 @@
-#include <Wire.h> 
+ #include <Wire.h> 
  
 #define BAUD (9600)    /* Serial baud define */
 #define _7SEG (0x38)   /* I2C address for 7-Segment */
@@ -54,6 +54,8 @@ void loop() {
   bool setArm = false;
   unsigned long setArmTime;
   unsigned long setDisarmTime;
+
+  bool standby = false;
   
   /* Configure 7-Segment to 12mA segment output current, Dynamic mode, 
      and Digits 1, 2, 3 AND 4 are NOT blanked */
@@ -99,6 +101,8 @@ void loop() {
     } else if (IncomingByte == 'd') {
       alarm = 'd';
       Serial.print("a: d\n");
+    } else if (IncomingByte == 't') {
+      standby = !standby;
     }
 
     // starts set arm countdown
@@ -135,8 +139,13 @@ void loop() {
     SerialMonitorPrint (Temperature_H, Decimal, IsPositive);
     
     /* Display temperature on the 7-Segment */
-    if (!setArm && alarm != 't') {
+    if (!setArm && alarm != 't' && !standby) {
       Dis_7SEG (Decimal, Temperature_H, Temperature_L, IsPositive, IsCelsius);
+    } else {
+      Send7SEG(4,0x00);
+      Send7SEG(3,0x00);
+      Send7SEG(2,0x00);
+      Send7SEG(1,0x00);
     }
     
     /* Alarm Stuff */
